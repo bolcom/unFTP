8b6f0fe1b181c8f0cdc091f9793864a93515a950
Hannes de Jager <hdejager@bol.com>
2019-03-11 11:23:37 +0100

    Implemented log shipping to a Redis host.

--- src/config.rs
+++ src/config.rs
@@ -1,29 +1,26 @@
 use std::env;
 
 /// Created to define and use the environment variables in use by Firetrap.
-pub enum EnvVar {
+pub enum Arg {
     NoDefault(&'static str),
     WithDefault(&'static str, &'static str),
 }
 
-impl EnvVar {
+impl Arg {
     pub fn name(&self) -> String {
         match self {
-            EnvVar::NoDefault(name) | EnvVar::WithDefault(name, _) => name.to_string(),
+            Arg::NoDefault(name) | Arg::WithDefault(name, _) => name.to_string(),
         }
     }
 
     pub fn val(&self) -> String {
         match self {
-            EnvVar::NoDefault(name) => env::var(name).unwrap(),
-            EnvVar::WithDefault(name, default) => env::var(name).unwrap_or(default.to_string()),
+            Arg::NoDefault(name) => env::var(name).unwrap(),
+            Arg::WithDefault(name, default) => env::var(name).unwrap_or_else(|_| default.to_string()),
         }
     }
 
     pub fn provided(&self) -> bool {
-        match env::var_os(self.name()) {
-            Some(_) => true,
-            None => false,
-        }
+        env::var_os(self.name()).is_some()
     }
 }
