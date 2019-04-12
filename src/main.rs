8ebd2c0b0ab35e607484650de6916ffe7b27bcc8
Hannes de Jager <hdejager@bol.com>
2019-04-12 15:30:22 +0200

    Implemented structured logging through slog

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod config;
 mod redislog;
 
+extern crate slog;
+extern crate slog_async;
+extern crate slog_term;
+
 use crate::config::Arg;
-use env_logger::Env;
 use firetrap::Server;
-use log::*;
 use std::env;
+
+use slog::*;
 
 const APP_NAME: &str = "unFTP";
 const APP_VERSION: &str = env!("BUILD_VERSION");
@@ -15,7 +19,7 @@ const ENV_LOG_REDIS_KEY: Arg = Arg::NoDefault("LOG_REDIS_KEY");
 const ENV_LOG_REDIS_HOST: Arg = Arg::WithDefault("LOG_REDIS_HOST", "localhost");
 const ENV_LOG_REDIS_PORT: Arg = Arg::WithDefault("LOG_REDIS_PORT", "6379");
 
-fn init_logging() {
+fn redis_logger() -> Option<redislog::Logger> {
     if ENV_LOG_REDIS_KEY.provided() {
         let logger = redislog::Builder::new(APP_NAME)
             .redis(
@@ -25,22 +29,25 @@ fn init_logging() {
             )
             .build()
             .expect("could not initialize Redis logger");
-
-        log::set_boxed_logger(Box::new(logger))
-            .map(|()| log::set_max_level(LevelFilter::Info))
-            .expect("could not set Redis logger");
-        info!("Redis logger initialized.");
-        return;
+        return Some(logger);
     }
-
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    info!("Env logger initialized.");
+    None
 }
 
 fn main() {
-    init_logging();
+    let drain = match redis_logger() {
+        Some(l) => slog_async::Async::new(l.fuse()).build().fuse(),
+        None => {
+            let decorator = slog_term::PlainDecorator::new(std::io::stdout());
+            let drain = slog_term::CompactFormat::new(decorator).build();
+            slog_async::Async::new(drain.fuse()).build().fuse()
+        }
+    };
+
+    let root = Logger::root(drain.fuse(), o!());
+    let log = root.new(o!("module" => "main"));
     let addr = ENV_UNFTP_ADDRESS.val();
     let server = Server::with_root(env::temp_dir()).greeting("Welcome to unFTP");
-    info!("Starting {} server version {} on {}.", APP_NAME, APP_VERSION, addr);
+    info!(log, "Starting {} server.", APP_NAME; "version" => APP_VERSION, "address" => &addr);
     server.listen(&addr);
 }
