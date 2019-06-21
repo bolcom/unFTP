e0c78ad7cb49c00bb389c3400df7315544b42b14
Steven Meunier <smeunier@bol.com>
2019-06-21 12:30:32 +0100

    Add Prometheus exporter for unFTP metrics

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 mod config;
 mod redislog;
 
+extern crate futures;
+extern crate hyper;
+extern crate prometheus;
 extern crate slog;
 extern crate slog_async;
 extern crate slog_term;
 
 use crate::config::Arg;
 use firetrap::Server;
+use futures::future;
+use hyper::rt::{self, Future};
+use hyper::service::service_fn;
+use hyper::{Body, Method, Request, Response, StatusCode};
+use prometheus::{Encoder, TextEncoder};
 use std::env;
+use std::thread;
 
 use slog::*;
 
@@ -18,6 +27,7 @@ const ENV_UNFTP_ADDRESS: Arg = Arg::WithDefault("UNFTP_ADDRESS", "0.0.0.0:2121")
 const ENV_UNFTP_HOME: Arg = Arg::NoDefault("UNFTP_HOME");
 const ENV_CERTS_FILE: Arg = Arg::NoDefault("CERTS_FILE");
 const ENV_KEY_FILE: Arg = Arg::NoDefault("KEY_FILE");
+const ENV_METRICS_ADDRESS: Arg = Arg::WithDefault("METRICS_ADDRESS", "0.0.0.0:9522"); // Re-use default port allocation of the TFTP Exporter
 const ENV_LOG_REDIS_KEY: Arg = Arg::NoDefault("LOG_REDIS_KEY");
 const ENV_LOG_REDIS_HOST: Arg = Arg::WithDefault("LOG_REDIS_HOST", "localhost");
 const ENV_LOG_REDIS_PORT: Arg = Arg::WithDefault("LOG_REDIS_PORT", "6379");
@@ -37,6 +47,30 @@ fn redis_logger() -> Option<redislog::Logger> {
     None
 }
 
+type BoxFuture = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+fn metrics_service(req: Request<Body>) -> BoxFuture {
+    let mut response = Response::new(Body::empty());
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/metrics") => {
+            *response.body_mut() = Body::from(gather_metrics());
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Box::new(future::ok(response))
+}
+
+fn gather_metrics() -> Vec<u8> {
+    let encoder = TextEncoder::new();
+    let metric_families = prometheus::gather();
+    let mut buffer = vec![];
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    return buffer;
+}
+
 fn main() {
     let drain = match redis_logger() {
         Some(l) => slog_async::Async::new(l.fuse()).build().fuse(),
@@ -49,11 +83,31 @@ fn main() {
 
     let root = Logger::root(drain.fuse(), o!());
     let log = root.new(o!("module" => "main"));
+
+    // HTTP server for exporting Prometheus metrics
+    let http_addr = ENV_METRICS_ADDRESS
+        .val()
+        .parse()
+        .expect(format!("Unable to parse metrics address {}", ENV_METRICS_ADDRESS.val()).as_str());
+    let http_log = log.clone();
+    let http_server = hyper::Server::bind(&http_addr)
+        .serve(|| service_fn(metrics_service))
+        .map_err(move |e| error!(http_log, "HTTP Server error: {}", e));
+    info!(log, "Starting Prometheus {} exporter.", APP_NAME; "address" => &http_addr);
+    let http_thread = thread::spawn(move || {
+        rt::run(http_server);
+    });
+
     let addr = ENV_UNFTP_ADDRESS.val();
     let home_dir = ENV_UNFTP_HOME.val_or_else(|_| env::temp_dir().as_path().to_str().unwrap().to_string());
     let use_ftps: bool = ENV_CERTS_FILE.provided() && ENV_KEY_FILE.provided();
     if !use_ftps && (ENV_CERTS_FILE.provided() || ENV_KEY_FILE.provided()) {
-        warn!(log, "Need to set both {} and {}. FTPS still disabled.", ENV_CERTS_FILE.name(), ENV_KEY_FILE.name())
+        warn!(
+            log,
+            "Need to set both {} and {}. FTPS still disabled.",
+            ENV_CERTS_FILE.name(),
+            ENV_KEY_FILE.name()
+        )
     }
 
     info!(log, "Starting {} server.", APP_NAME;
@@ -61,6 +115,15 @@ fn main() {
     "address" => &addr,
     "home" => home_dir.clone());
 
-    let server = Server::with_root(home_dir).greeting("Welcome to unFTP");
-    server.listen(&addr);
+    let server = Server::with_root(home_dir).greeting("Welcome to unFTP").with_metrics();
+    let ftp_thread = thread::spawn(move || {
+        server.listen(&addr);
+    });
+
+    http_thread
+        .join()
+        .expect(format!("The Prometheus {} exporter server thread has panicked", APP_NAME).as_str());
+    ftp_thread
+        .join()
+        .expect(format!("The {} server thread has panicked", APP_NAME).as_str());
 }
