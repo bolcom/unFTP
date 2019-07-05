3204b329d3d80ed2afc1ba34cfd7fb7801200c8c
Hannes de Jager <hdejager@bol.com>
2019-07-05 14:09:46 +0200

    Preparation for FTPS: define and read environment variables for certs & private key files.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ const APP_VERSION: &str = env!("BUILD_VERSION");
 
 const ENV_UNFTP_ADDRESS: Arg = Arg::WithDefault("UNFTP_ADDRESS", "0.0.0.0:2121");
 const ENV_UNFTP_HOME: Arg = Arg::NoDefault("UNFTP_HOME");
+const ENV_CERTS_FILE: Arg = Arg::NoDefault("CERTS_FILE");
+const ENV_KEY_FILE: Arg = Arg::NoDefault("KEY_FILE");
 const ENV_LOG_REDIS_KEY: Arg = Arg::NoDefault("LOG_REDIS_KEY");
 const ENV_LOG_REDIS_HOST: Arg = Arg::WithDefault("LOG_REDIS_HOST", "localhost");
 const ENV_LOG_REDIS_PORT: Arg = Arg::WithDefault("LOG_REDIS_PORT", "6379");
@@ -48,12 +50,17 @@ fn main() {
     let root = Logger::root(drain.fuse(), o!());
     let log = root.new(o!("module" => "main"));
     let addr = ENV_UNFTP_ADDRESS.val();
-
     let home_dir = ENV_UNFTP_HOME.val_or_else(|_| env::temp_dir().as_path().to_str().unwrap().to_string());
-    let server = Server::with_root(home_dir.clone()).greeting("Welcome to unFTP");
+    let use_ftps: bool = ENV_CERTS_FILE.provided() && ENV_KEY_FILE.provided();
+    if !use_ftps && (ENV_CERTS_FILE.provided() || ENV_KEY_FILE.provided()) {
+        warn!(log, "Need to set both {} and {}. FTPS still disabled.", ENV_CERTS_FILE.name(), ENV_KEY_FILE.name())
+    }
+
     info!(log, "Starting {} server.", APP_NAME;
     "version" => APP_VERSION,
     "address" => &addr,
-    "home" => home_dir);
+    "home" => home_dir.clone());
+
+    let server = Server::with_root(home_dir).greeting("Welcome to unFTP");
     server.listen(&addr);
 }
