6676a4f47abd03b15995ee8e848b70ea4501094b
Hannes de Jager <hdejager@bol.com>
2020-07-10 20:37:27 +0200

    Added service home page (#56)

--- src/http.rs
+++ src/http.rs
@@ -8,6 +8,11 @@ use hyper::{
 use slog::*;
 use std::{net::SocketAddr, result::Result};
 
+const PATH_HOME: &str = "/";
+const PATH_METRICS: &str = "/metrics";
+const PATH_HEALTH: &str = "/health";
+const PATH_READINESS: &str = "/ready";
+
 // starts an HTTP server and exports Prometheus metrics.
 pub async fn start(log: &Logger, bind_addr: &str) -> Result<(), String> {
     let http_addr: SocketAddr = bind_addr
@@ -17,25 +22,45 @@ pub async fn start(log: &Logger, bind_addr: &str) -> Result<(), String> {
     let make_svc = make_service_fn(|_conn| {
         async {
             // service_fn converts our function into a `Service`
-            Ok::<_, hyper::Error>(service_fn(metrics_service))
+            Ok::<_, hyper::Error>(service_fn(router))
         }
     });
 
     let http_server = hyper::Server::bind(&http_addr).serve(make_svc);
 
-    info!(log, "Starting Prometheus {} exporter.", app::NAME; "address" => &http_addr);
+    info!(log, "Starting HTTP service."; "address" => &http_addr);
+    info!(log, "Exposing {} service home.", app::NAME; "path" => PATH_HOME);
+    info!(log, "Exposing Prometheus {} exporter endpoint.", app::NAME; "path" => PATH_METRICS);
+    info!(log, "Exposing readiness endpoint."; "path" => PATH_READINESS);
+    info!(log, "Exposing liveness endpoint."; "path" => PATH_HEALTH);
 
     if let Err(e) = http_server.await {
-        error!(log, "HTTP Server error: {}", e)
+        error!(log, "HTTP server error: {}", e)
     }
     Ok(())
 }
 
-async fn metrics_service(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let mut response = Response::new(Body::empty());
+fn service_home() -> Body {
+    let index_html = include_str!(concat!(env!("PROJ_WEB_DIR"), "/index.html"));
+    Body::from(index_html.replace("{{ .AppVersion }}", app::VERSION))
+}
+
+async fn router(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let mut response: Response<Body> = Response::new(Body::empty());
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/metrics") => {
+        (&Method::GET, PATH_HOME) | (&Method::GET, "/index.html") => {
+            *response.body_mut() = service_home();
+        }
+        (&Method::GET, PATH_METRICS) => {
             *response.body_mut() = Body::from(metrics::gather());
+        }
+        (&Method::GET, PATH_READINESS) => {
+            *response.body_mut() = Body::from("<html>Ready!</html>");
+            *response.status_mut() = StatusCode::OK;
+        }
+        (&Method::GET, PATH_HEALTH) => {
+            *response.body_mut() = Body::from("<html>OK!</html>");
+            *response.status_mut() = StatusCode::OK;
         }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
